d8d0586a6e60b33dde9db20c3a4640fc61c0806f
Mark <MarkKleinfelder@gmail.com>
2018-02-01 18:11:52 -0700

    working on button creation and css class addiotion

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import $ from 'jquery';
 window.jQuery = window.$ = $;
+//var buttonArray=[];
+var STEPS=8;
 
-//var STEPS=8;
+// var sounds = [
+//   'hihatAnalog.wav',
+//   'openhatTight.wav',
+//   'kickFloppy.wav',
+//   'kickHeavy.wav',
+//   'snareAnalog.wav',
+//   'snareBlock.wav'
+// ];
+
+
 
 function BeatButton(props){
   return (
-    <button className='beat' onClick={props.onClick}>
-      
+    <button className='beat' onClick={props.onClick}>      
     </button>
   );
 }
@@ -20,34 +30,55 @@ class Board extends Component {
   constructor(props){
     super(props);
     this.state = {
+      buttonArray:Array(STEPS).fill(false),
       isBeat: false,
-      // buttons:Array(STEPS).addClass('beat'),
     }
   }
 
   handleClick(i){
-    console.log('handleClick hit' + JSON.stringify(this.state))
+    const buttonArray = this.state.buttonArray.slice();
+    buttonArray[i]=!buttonArray[i];
+    // if(buttonArray[i]===false){
+    //   buttonArray[i]=true;
+    // }else{
+    //   buttonArray[i]=false;
+    // }
     this.setState({
-      isBeat:!this.state.isBeat,
+      buttonArray:buttonArray,
+      isBeat:buttonArray[i],
+      beatClass:buttonArray[i] ? 'beat on' : 'beat',
     })
-
-    
+    console.log('handleClick hit ' + buttonArray)
+    console.log("buttonArray value " + buttonArray[i])
+    //console.log('beatClass ' + this.state.beatClass)
   }
 
   renderBeatButton(i){
     return(
       <BeatButton 
+        className={this.state.beatClass}
         onClick={()=> this.handleClick(i)}
+        key={i}
       />
     );
   }
 
+  renderAllButtons(){
+    this.buttonArray=[];
+    for(var i = 0; i<STEPS; i++){
+      let newButton = this.renderBeatButton(i);
+      this.buttonArray.push(newButton);
+    }
+    return;
+  }
+
   render() {
-    console.log('app.js is running!');   
+    console.log('app.js is running!');
+    this.renderAllButtons();   
     return ( 
       <div className='container'> 
         <div className='item machineGrid'>
-          {this.renderBeatButton(0)}
+          {this.buttonArray}
         </div>  
       </div>
     );
